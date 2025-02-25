9b528255283b5d63a38ce649c9ed7afa58059a7c
Samul Ovalle <sfovallerodriguez@gmail.com>
2025-02-25 17:28:17 -0500

    The files to be used in SkyGreen were adjusted, and the process of changing the local repository was completed.

--- archivos/js/index_script.js
+++ archivos/js/index_script.js
@@ -1,49 +1,18 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 /* 
 ======================================
 File: opening_creen.js
 Description: 
 Autor: Samuel Felipe Ovalle Rodriguez
-<<<<<<< HEAD
-Last modification: 24/2/2025
-======================================
-*/
-
-import {blur_effect, dropdown_panels, scroll_dropdown_panels} from "./header_funtions.js";
-=======
 Last modification: 25/2/2025
 ======================================
 */
 import {dropdown, scroll_dropdown_panels} from "./header_funtions.js";
->>>>>>> master
 window.addEventListener("load", ()=>{
     const opening_screen = document.getElementById("opening_screen");
     opening_screen.scrollIntoView({ behavior: "auto" });
     history.scrollRestoration = "manual";
     
-<<<<<<< HEAD
-    const icon_menu = document.querySelector(".icon_menu");
-    const icon_shopping = document.querySelector(".icon_shopping");
-    let dropdown_acrive;
-    let dropdown_status = false;
-    icon_menu.addEventListener("click", ()=>{
-        dropdown_acrive = ".menu";
-        dropdown_status = dropdown_panels(dropdown_acrive, "73vw", dropdown_status);
-        blur_effect(dropdown_status);        
-    })
-    icon_shopping.addEventListener("click", ()=>{
-        dropdown_acrive = ".shopping";
-        dropdown_status = dropdown_panels(dropdown_acrive,"60vw", dropdown_status);
-        blur_effect(dropdown_status);
-    })
-
-    let screen_position = 0;
-    window.addEventListener("wheel", (e) => {  
-        let cursor_status = scroll_dropdown_panels(dropdown_status, dropdown_acrive);        
-=======
     dropdown("menu", "73vw", 500)
     dropdown("shopping", "60vw", 500)
 
@@ -51,7 +20,6 @@ window.addEventListener("load", ()=>{
     let screen_position = 0;
     window.addEventListener("wheel", (e) => {  
         let cursor_status = scroll_dropdown_panels();        
->>>>>>> master
         if (e.deltaY > 0) {
             // down
             if (screen_position < 100 &&  cursor_status == true) {opening_screen.style.bottom = `${screen_position = screen_position + 10}%`}
